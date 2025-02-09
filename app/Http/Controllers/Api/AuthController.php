98d3cb41b287965c85a40b647c58ee4e0b7fdd0e
Pelepoy <philipmarcel.maratas@gmail.com>
2025-02-09 14:24:24 +0800

    add auth controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,20 +6,33 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use App\Notifications\EmailVerificationNotification;
 use GrahamCampbell\ResultType\Success;
+use Ichtrojan\Otp\Otp;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    protected $otp;
+    public function __construct()
+    {
+        $this->otp = new Otp();
+    }
+    
     public function register(RegisterRequest $request)
     {
         $user = User::create($request->validated());
         $token = $user->createToken($request->first_name)->plainTextToken;
+        $otp = $this->otp->generate($user->email, 'numeric', 6, 5);
 
+        // Send OTP to user's email
+        $user->notify(new EmailVerificationNotification($otp->token));
+        
         return response()->json([
             'status' => 'success',
-            'message' => 'Registration successful',
+            'message' => 'Registration Successful. OTP sent to your email.',
+            'redirect' => env('APP_ENV'). '/api/email-verification',
             'token' => $token
         ], 201);
     }
