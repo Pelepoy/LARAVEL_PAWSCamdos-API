7504a3d2ebbc12fee74f796646775b47be98b582
Pelepoy <philipmarcel.maratas@gmail.com>
2025-02-09 14:28:33 +0800

    initial set up for api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\DogController;
+use App\Http\Controllers\EmailVerificationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,8 @@ Route::middleware('auth:sanctum')->group(function () {
 	Route::get('/v1/auth', function (Request $request) {
 		return $request->user();
 	});
+	Route::post('/email-verification', [EmailVerificationController::class, 'email_verification']);
+	Route::get('/resend-otp', [EmailVerificationController::class, 'resend_otp']);
 	Route::post('/v1/logout', [AuthController::class, 'logout']);
 });
 
@@ -24,4 +27,4 @@ Route::middleware('auth:sanctum')->group(function () {
  * @Resource Routes
  */
 
-Route::apiResource('/v1/dogs-info', DogController::class);
+Route::apiResource('/v1/dogs', DogController::class);
