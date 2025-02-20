85f40484c50a841759eb17a7dd01c2cc16503578
Pelepoy <philipmarcel.maratas@gmail.com>
2025-02-20 18:30:10 +0800

    add pet factory that creates a pet info with the it's associated owner

--- database/factories/PetFactory.php
+++ database/factories/PetFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,22 @@ class PetFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'owner_id' => User::inRandomOrder()->first()?->id ?? User::factory(), // Associate with a random user or create one
+            'species' => fake()->randomElement(['Dog', 'Cat', 'Rabbit', 'Bird', 'Fish']),
+            'name' => fake()->firstName(),
+            'breed' => fake()->word(),
+            'color' => fake()->safeColorName(),
+            'age' => fake()->numberBetween(1, 20),
+            'weight' => fake()->randomFloat(2, 1, 50), // Random weight between 1kg and 50kg
+            'description' => fake()->sentence(),
+            'gender' => fake()->randomElement(['male', 'female']),
+            'date_of_birth' => fake()->date(),
+            'microchip_id' => fake()->optional()->uuid(),
+            'insurance_policy_number' => fake()->optional()->bothify('POL-####-####'),
+            'is_vaccinated' => fake()->boolean(),
+            'is_neutered' => fake()->boolean(),
+            'file_name' => fake()->optional()->word() . '.jpg',
+            'profile_image_url' => fake()->imageUrl(200, 200, 'animals', true, 'pets'), // Random pet image
         ];
     }
 }
