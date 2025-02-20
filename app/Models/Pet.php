6d7e0010e55c999bc4a9d4fc18fa01ca867b8605
Pelepoy <philipmarcel.maratas@gmail.com>
2025-02-20 18:25:52 +0800

    add boot function to clear the image in storage after  force deleting and add file_name field in fillable array

--- app/Models/Pet.php
+++ app/Models/Pet.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class Pet extends Model
 {
@@ -25,11 +27,25 @@ class Pet extends Model
         'insurance_policy_number',
         'is_vaccinated',
         'is_neutered',
+        'file_name',
         'profile_image_url',
     ];
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    protected static function boot()
+    {
+    parent::boot();
+
+    // Delete the associated image file when the pet is force deleted
+    static::forceDeleting(function ($pet) {
+        if ($pet->file_name) {
+            // Log::info("Deleting file: {$pet->file_name}");
+            Storage::disk()->delete($pet->file_name);
+            }
+        });
     }
 }
