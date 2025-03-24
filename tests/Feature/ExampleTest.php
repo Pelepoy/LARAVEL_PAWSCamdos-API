ffa809ad86a100367b9a946004ebb8e09869fadf
Pelepoy <philipmarcel.maratas@gmail.com>
2025-03-24 14:06:52 +0800

    add test example

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Pet;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -12,7 +13,7 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/v1/pets/cursor-paginate');
 
         $response->assertStatus(200);
     }
